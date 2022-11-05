50e219274cde8bca71f8d1449cf8dea2bc2ceed3
abrahamduno <abraham.altmail@gmail.com>
2022-11-05 13:28:26 -0400

    modular html

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ import ScrollWatcher from "./res/js/scroll.js";
 import VueLike from "./vuelike.js";
 import * as THREE from "three";
 
+// import footerhtml from "./include/footer.html";
 import { MTLLoader } from "./res/loaders/MTLLoader.js";
 // import { OBJLoader } from "./res/loaders/OBJLoader.js";
 
@@ -41,7 +42,41 @@ function _animate() {
 
 const TheScene = new MyScene({DEBUG:false});
 
+const loadElement = (_file, _element) => {
+  // document.querySelector('footer').append(footerhtml);
+
+  let xhttp;
+  let element = document.getElementById(_element)
+
+  if (_file)
+  {
+    xhttp = new XMLHttpRequest()
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4)
+      {
+          if (this.status == 200) {
+            element.innerHTML = this.responseText
+          }
+          if (this.status == 404) { element.innerHTML = `<div>404 ${_element} 404</div>` }
+      }
+    }
+    xhttp.open("GET", _file, true)
+    xhttp.send()
+    return
+  }
+
+    // fetch('./include/footer.inc')
+    // .then ( (r) => { return r.text();  } )
+    // .then ( (s) => {
+    //         p= new DOMParser();
+    //         d = p.parseFromString(s,'text/html') ;
+    // });
+};
+
 $(document).ready(() => {
+  loadElement('./include/ourwork-1.html', "ourwork-1")
+  loadElement('./include/ourwork-2.html', "ourwork-2")
+
 	const newjQueryTemplateView = new VueLike('#templateViewId', {}, TheScene);
   const TheScrollWatcher = new ScrollWatcher(TheScene, newjQueryTemplateView);
   function scrollUpdater(event) { TheScrollWatcher.update() }
